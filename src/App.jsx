3746c68b440536cfd42e477cf7497cacc47e5111
André <andregparada@gmail.com>
2024-04-11 14:05:03 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ab odit nulla illo" 
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post"
+      />
     </div>
   )
 }
