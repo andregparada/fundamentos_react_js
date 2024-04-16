2596b3fe176b73a93f179a67a18d061277d003dc
André <andregparada@gmail.com>
2024-04-16 15:54:25 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Diego Fernandes"
         content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ab odit nulla illo" 
