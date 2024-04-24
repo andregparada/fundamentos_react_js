51284821b4879604bbf008a76885fb169516423e
André <andregparada@gmail.com>
2024-04-24 18:54:32 -0300

    Component: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ab odit nulla illo" 
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="Um novo post"
-      />
+      
+      <div className={styles.wrapper} >
+        <Sidebar />
+        <main>
+          <Post
+          author="Diego Fernandes"
+          content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat ab odit nulla illo" 
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post"
+          />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1626699975537-008c47ee2ed8?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  
+            />
+            <div className={styles.profile}>
+                <strong>André Parada</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">Editar seu perfil</a>
+            </footer>
+        </aside>
+    );
+}
