5d64fa12f291c37a5680a017d1d131afa0a91d21
André <andregparada@gmail.com>
2024-06-17 13:52:42 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -10,8 +11,7 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1626699975537-008c47ee2ed8?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"  
             />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/andregparada.png" />
-
+                <Avatar src="https://github.com/andregparada.png" />
                 <strong>André Parada</strong>
                 <span>Web Developer</span>
             </div>
